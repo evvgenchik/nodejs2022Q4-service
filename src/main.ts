593b67162a7301447dce477f3c7626aeeadbb23c
Evgen <yevgenchikk@mail.ru>
2023-06-05 18:57:24 +0300

    feat: add basic doc

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 4001;
   const app = await NestFactory.create(AppModule);
 
-  // const config = new DocumentBuilder().setTitle('Api').build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('/api/docs', app, document);
+  const pathToFile = path.join(__dirname, '../doc/doc.json');
+  const document = JSON.parse(readFileSync(pathToFile) as unknown as string);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
